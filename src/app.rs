6bebd1d8310a8eb130b4831f84f1711e21ee0a84
Nederg <ergxned@proton.me>
2024-05-30 20:35:10 +0200

    changed the layout, improved charts and colors

--- src/app.rs
+++ src/app.rs
@@ -21,9 +21,8 @@ impl App {
             if poll(Duration::from_millis(1000))? {
                 self.handle_events()?;
             }
-
-            terminal.draw(|frame| self.render_frame(frame))?;
             self.update_chart_data();
+            terminal.draw(|frame| self.render_frame(frame))?;
         }
         Ok(())
     }
@@ -32,7 +31,7 @@ impl App {
         let main_layout = Layout::new(
             Direction::Vertical,
             [
-                Constraint::Length(2),
+                Constraint::Length(1),
                 Constraint::Min(0),
                 Constraint::Length(1),
             ],
@@ -44,7 +43,8 @@ impl App {
                 .borders(Borders::TOP)
                 .title(" Sigmanauts Mining Pool ")
                 .title_alignment(Alignment::Center)
-                .title_style(Style::default().fg(Color::LightYellow)),
+                .title_style(Style::default().fg(Color::Green))
+                .border_style(Style::fg(Style::default().fg(Color::Green), Color::Green)),
             main_layout[0],
         );
 
@@ -53,15 +53,10 @@ impl App {
                 .borders(Borders::TOP)
                 .title(" v0.0.1 ")
                 .title_alignment(Alignment::Right)
-                .title_style(Color::Green),
+                .title_style(Color::Green)
+                .border_style(Style::fg(Style::default().fg(Color::Green), Color::Green)),
             main_layout[2],
         );
-
-        let inner_layout = Layout::new(
-            Direction::Horizontal,
-            [Constraint::Percentage(50), Constraint::Percentage(50)],
-        )
-        .split(main_layout[1]);
 
         let stats_layout = Layout::new(
             Direction::Vertical,
@@ -71,55 +66,86 @@ impl App {
                 Constraint::Percentage(33),
             ],
         )
-        .split(inner_layout[0]);
+        .split(main_layout[1]);
 
-        frame.render_widget(Block::bordered().title(" Network Stats "), stats_layout[0]);
+        let network_layout = Layout::new(
+            Direction::Horizontal,
+            [Constraint::Percentage(50), Constraint::Percentage(50)],
+        )
+        .margin(1)
+        .split(stats_layout[0]);
+
+        let pool_layout = Layout::new(
+            Direction::Horizontal,
+            [Constraint::Percentage(50), Constraint::Percentage(50)],
+        )
+        .margin(1)
+        .split(stats_layout[1]);
+
+        let miner_layout = Layout::new(
+            Direction::Horizontal,
+            [Constraint::Percentage(50), Constraint::Percentage(50)],
+        )
+        .margin(1)
+        .split(stats_layout[2]);
+
         frame.render_widget(
-            Block::bordered().title(self.counter.to_string().yellow()),
+            Block::bordered()
+                .title(" Network Stats ")
+                .border_style(Style::fg(Style::default().fg(Color::Green), Color::Green)),
+            stats_layout[0],
+        );
+        frame.render_widget(
+            Block::bordered()
+                .title(" Pool Stats ".green())
+                .border_style(Style::fg(Style::default().fg(Color::Green), Color::Green)),
             stats_layout[1],
         );
-        frame.render_widget(Block::bordered().title(" Miner Stats "), stats_layout[2]);
-
-        let chart_layout = Layout::new(
-            Direction::Vertical,
-            [
-                Constraint::Percentage(33),
-                Constraint::Percentage(33),
-                Constraint::Percentage(33),
-            ],
-        )
-        .split(inner_layout[1]);
+        frame.render_widget(
+            Block::bordered()
+                .title(" Miner Stats ")
+                .border_style(Style::fg(Style::default().fg(Color::Green), Color::Green)),
+            stats_layout[2],
+        );
 
         frame.render_widget(
             self.render_chart(
                 "Network Hashrate",
-                Style::default().cyan(),
+                "Blocks",
+                "Th/s",
+                Style::default().white(),
                 &self.network_hashrate.clone(),
             ),
-            chart_layout[0],
+            network_layout[1],
         );
 
         frame.render_widget(
             self.render_chart(
                 "Pool Hashrate",
-                Style::default().magenta(),
+                "Blocks",
+                "Th/s",
+                Style::default().white(),
                 &self.pool_hashrate.clone(),
             ),
-            chart_layout[1],
+            pool_layout[1],
         );
         frame.render_widget(
             self.render_chart(
                 "Miner Hashrate",
-                Style::default().light_green(),
+                "Time",
+                "Mh/s",
+                Style::default().white(),
                 &self.miner_hashrate.clone(),
             ),
-            chart_layout[2],
+            miner_layout[1],
         );
     }
 
     fn render_chart<'a>(
         &'a self,
         name: &'static str,
+        x_axis_title: &'static str,
+        y_axis_title: &'static str,
         style: Style,
         data: &'a Vec<(f64, f64)>,
     ) -> Chart {
@@ -128,29 +154,59 @@ impl App {
             // Line chart
             Dataset::default()
                 .name(name)
-                .marker(symbols::Marker::Braille)
+                .marker(symbols::Marker::Block)
                 .graph_type(GraphType::Line)
                 .style(style)
                 .data(data),
         ];
 
+        let min_value_x = data
+            .iter()
+            .map(|&(x, _)| x)
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+
+        let max_value_x = data
+            .iter()
+            .map(|&(x, _)| x)
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+
         // Create the X axis and define its properties
         let x_axis = Axis::default()
-            .title("X Axis".red())
-            .style(Style::default().white())
-            .bounds([0.0, 5.0])
-            .labels(vec!["0.0".into(), "2.5".into(), "5.0".into()]);
+            .title(x_axis_title.green())
+            .style(Style::default().green())
+            .bounds([min_value_x, max_value_x])
+            .labels(vec![
+                min_value_x.to_string().into(),
+                ((min_value_x + max_value_x) / 2.0).to_string().into(),
+                max_value_x.to_string().into(),
+            ]);
+
+        let min_value_y = data
+            .iter()
+            .map(|&(_, y)| y)
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+
+        let max_value_y = data
+            .iter()
+            .map(|&(_, y)| y)
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
 
         // Create the Y axis and define its properties
         let y_axis = Axis::default()
-            .title("Y Axis".red())
-            .style(Style::default().white())
-            .bounds([5.0, 25.0])
+            .title(y_axis_title.green())
+            .style(Style::default().green())
+            .bounds([
+                min_value_y - (min_value_y * 0.1),
+                max_value_y + (max_value_y * 0.1),
+            ])
             .labels(vec![
-                "5.0".into(),
-                "10.0".into(),
-                "15.0".into(),
-                "20.0".into(),
+                (min_value_y - (min_value_y * 0.1)).to_string().into(),
+                ((min_value_y + max_value_y) / 2.0).to_string().into(),
+                (max_value_y + (max_value_y * 0.1)).to_string().into(),
             ]);
 
         // Create the chart and link all the parts together
